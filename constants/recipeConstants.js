fe69355840b44bd349cf2609345e065b2aef6dd9
Topaz Aakal <topazaakal5@gmail.com>
2024-08-16 09:48:18 +0300

    clean code

---
+++ constants/recipeConstants.js
@@ -0,0 +1,27 @@
+export const unitOptions = [
+  { label: 'מ"ל', value: 'מ"ל' },
+  { label: "ליטר", value: "ליטר" },
+  { label: 'מ"ג', value: 'מ"ג' },
+  { label: "גרם", value: "גרם" },
+  { label: 'ק"ג', value: "קילוגרם" },
+  { label: "כוס", value: "כוס" },
+  { label: "כף", value: "כף" },
+  { label: "כפית", value: "כפית" },
+  { label: "יחידה", value: "יחידה" },
+  { label: "קורט", value: "קורט" },
+];
+
+export const departments = [
+  { name: "פירות וירקות", image: require("../images/fruits.png") },
+  { name: "בשר ועוף", image: require("../images/chicken.png") },
+  { name: "דגים", image: require("../images/fish.png") },
+  { name: "תבלינים", image: require("../images/spices.png") },
+  { name: "מוצרי חלב וביצים", image: require("../images/milk-eggs.png") },
+  { name: "ממרחים", image: require("../images/sauces.png") },
+  { name: "קפה ותה", image: require("../images/coffee.png") },
+  { name: "ממתקים", image: require("../images/sweets.png") },
+  { name: "אלכוהול", image: require("../images/alcohol.png") },
+  { name: "שימורים", image: require("../images/cans.png") },
+  { name: "לחם", image: require("../images/bread.png") },
+  { name: "אחר", image: require("../images/other.png") },
+];
--- screens/ShoppingListScreen.js
+++ screens/ShoppingListScreen.js
@@ -19,6 +19,7 @@ import * as FileSystem from "expo-file-system";
 import { formatUnit } from "../util/unitConversion";
 import Feather from "@expo/vector-icons/Feather";
 import CustomModalDropdown from "../components/UI/CustomModalDropdown";
+import { unitOptions, departments } from "../constants/recipeConstants";
 
 import {
   fetchRecipeById,
@@ -27,34 +28,6 @@ import {
   clearShoppingList,
   deleteShoppingListItems,
 } from "../util/database";
-
-const unitOptions = [
-  { label: 'מ"ל', value: 'מ"ל' },
-  { label: "ליטר", value: "ליטר" },
-  { label: 'מ"ג', value: 'מ"ג' },
-  { label: "גרם", value: "גרם" },
-  { label: 'ק"ג', value: "קילוגרם" },
-  { label: "כוס", value: "כוס" },
-  { label: "כף", value: "כף" },
-  { label: "כפית", value: "כפית" },
-  { label: "יחידה", value: "יחידה" },
-  { label: "קורט", value: "קורט" },
-];
-
-const departments = [
-  { name: "פירות וירקות", image: require("../images/fruits.png") },
-  { name: "בשר ועוף", image: require("../images/chicken.png") },
-  { name: "דגים", image: require("../images/fish.png") },
-  { name: "תבלינים", image: require("../images/spices.png") },
-  { name: "מוצרי חלב וביצים", image: require("../images/milk-eggs.png") },
-  { name: "ממרחים", image: require("../images/sauces.png") },
-  { name: "קפה ותה", image: require("../images/coffee.png") },
-  { name: "ממתקים", image: require("../images/sweets.png") },
-  { name: "אלכוהול", image: require("../images/alcohol.png") },
-  { name: "שימורים", image: require("../images/cans.png") },
-  { name: "לחם", image: require("../images/bread.png") },
-  { name: "אחר", image: require("../images/other.png") },
-];
 
 const departmentOrder = departments.map((department) => department.name);
 
